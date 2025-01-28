6e8c9537386d6d7b8a35adf7f1418d60e3bd2eb8
Muhammad Adnan Noor <116208763+AdnanDevp@users.noreply.github.com>
2025-01-28 14:02:16 +0100

    Add support for multiple Plotly Express datasets

--- src/libraries/group_data.py
+++ src/libraries/group_data.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from sklearn.decomposition import TruncatedSVD, PCA
 
-import plotly.express as plx
+import plotly.express as px
 
 import copy
 import os
@@ -17,13 +17,34 @@ from functools import wraps
 class Group_Data: 
     """Have all data grouping shizzle combined in one class
     """
+    # Available datasets and their numeric columns
+    available_datasets = {
+        'iris': {'data': px.data.iris, 'numeric_cols': ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']},
+        'tips': {'data': px.data.tips, 'numeric_cols': ['total_bill', 'tip', 'size']},
+        'gapminder': {'data': px.data.gapminder, 'numeric_cols': ['lifeExp', 'pop', 'gdpPercap']},
+        'wind': {'data': px.data.wind, 'numeric_cols': ['direction', 'strength']},
+        'stocks': {'data': px.data.stocks, 'numeric_cols': ['GOOG', 'AAPL', 'AMZN', 'FB', 'NFLX', 'MSFT']}
+    }
+
     ## Get dimension reduction data cloud
     def get_dimension_reduction(self, visualize_data):
+        # Get the selected dataset
+        dataset_name = visualize_data.get("selectedDataset", "iris")
+        dataset_info = self.available_datasets[dataset_name]
+        
+        # Load the dataset
+        df = dataset_info['data']()
+        
+        # Get numeric columns for the selected dataset
+        X = df[dataset_info['numeric_cols']]
 
-        X = plx.data.iris().iloc[:, :4]
-
+        # Perform PCA
         pca = PCA(n_components=visualize_data["visualizeDimensionReductionN"], random_state=17)
-
         X_plot = pd.DataFrame(pca.fit_transform(X), index=X.index)
 
         return X_plot
+
+    @classmethod
+    def get_available_datasets(cls):
+        """Return list of available dataset names"""
+        return list(cls.available_datasets.keys())
