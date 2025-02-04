0b742a85cc5a26319eaf4a00d6e755ca132a79c6
Muhammad Adnan Noor <116208763+AdnanDevp@users.noreply.github.com>
2025-02-04 09:27:12 +0100

    Add support for multiple plotly.express datasets

--- src/libraries/group_data.py
+++ src/libraries/group_data.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from sklearn.decomposition import TruncatedSVD, PCA
 
-import plotly.express as plx
+import plotly.express as px
 
 import copy
 import os
@@ -17,10 +17,27 @@ from functools import wraps
 class Group_Data: 
     """Have all data grouping shizzle combined in one class
     """
+    # Available datasets from plotly.express
+    available_datasets = {
+        "iris": {"func": px.data.iris, "numeric_cols": ["sepal_length", "sepal_width", "petal_length", "petal_width"]},
+        "gapminder": {"func": px.data.gapminder, "numeric_cols": ["lifeExp", "pop", "gdpPercap"]},
+        "tips": {"func": px.data.tips, "numeric_cols": ["total_bill", "tip", "size"]},
+        "wines": {"func": px.data.wine, "numeric_cols": ["alcohol", "malic_acid", "ash", "alcalinity_of_ash", "magnesium", 
+                                                      "total_phenols", "flavanoids", "nonflavanoid_phenols", "proanthocyanins", 
+                                                      "color_intensity", "hue", "od280/od315_of_diluted_wines", "proline"]}
+    }
+
     ## Get dimension reduction data cloud
     def get_dimension_reduction(self, visualize_data):
-
-        X = plx.data.iris().iloc[:, :4]
+        # Get the selected dataset
+        dataset_name = visualize_data.get("selectedDataset", "iris")
+        dataset_info = self.available_datasets[dataset_name]
+        
+        # Load the dataset
+        df = dataset_info["func"]()
+        
+        # Select numeric columns for PCA
+        X = df[dataset_info["numeric_cols"]]
 
         pca = PCA(n_components=visualize_data["visualizeDimensionReductionN"], random_state=17)
 
